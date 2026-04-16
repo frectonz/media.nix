150398dac7383732d58192f63150c750f6897eb4
frectonz <fraol0912@gmail.com>
2026-04-16 13:06:59 +0300

    fix: remove media user

--- hosts/curie/configuration.nix
+++ hosts/curie/configuration.nix
@@ -22,16 +22,6 @@
     "${flake}/users/chuchu/authorized_keys"
   ];
 
-  users.users.media = {
-    isNormalUser = true;
-    description = "media";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-    packages = [ ];
-  };
-
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.grub.enable = true;
